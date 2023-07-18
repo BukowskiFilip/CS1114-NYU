72cfa0c5cdac0e1fcee9e728c648ca5ad063f507
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2023-07-18 18:55:41 -0400

    Update hw1_q2.py

--- CS1114_HW1/hw1_q2.py
+++ CS1114_HW1/hw1_q2.py
@@ -1,12 +1,7 @@
-"""
-Author: [Filip Bukowski]
-Assignment / Part: HW1 - Q1 (etc.)
-Date due: 2022-09-22, 11:59pm
-I pledge that I have completed this assignment without
-collaborating with anyone else, in conformance with the
-NYU School of Engineering Policies and Procedures on
-Academic Misconduct.
-"""
+#code is correct but must let pythom do the math for you
+#1/2
+
+
 
 user_year = int(input("Please enter a year greater than 2022: "))
 
