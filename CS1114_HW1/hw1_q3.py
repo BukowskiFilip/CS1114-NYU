2f4fb740ae98443c320ce6e9a615437ab4211b7f
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2023-07-18 19:00:22 -0400

    Update hw1_q3.py

--- CS1114_HW1/hw1_q3.py
+++ CS1114_HW1/hw1_q3.py
@@ -1,12 +1,8 @@
-"""
-Author: [Filip Bukowski]
-Assignment / Part: HW1 - Q1 (etc.)
-Date due: 2022-09-22, 11:59pm
-I pledge that I have completed this assignment without
-collaborating with anyone else, in conformance with the
-NYU School of Engineering Policies and Procedures on
-Academic Misconduct.
-"""
+#question correct 2/2
+
+
+
+
 
 print("Please enter number of coins")
 user_quarters = int(input("Number of quarters: "))
