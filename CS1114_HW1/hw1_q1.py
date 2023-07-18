b01174860b907f65e7b83f8a8f346972f5dd35ce
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2023-07-18 18:46:54 -0400

    Update hw1_q1.py

--- CS1114_HW1/hw1_q1.py
+++ CS1114_HW1/hw1_q1.py
@@ -1,12 +1,4 @@
-"""
-Author: [Filip Bukowski]
-Assignment / Part: HW1 - Q1 (etc.)
-Date due: 2022-09-22, 11:59pm
-I pledge that I have completed this assignment without
-collaborating with anyone else, in conformance with the
-NYU School of Engineering Policies and Procedures on
-Academic Misconduct.
-"""
+#no errors 2/2
 
 
 
