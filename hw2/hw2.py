a544597b7deef6ee15d41634b9eae80b1ccaa4b8
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 18:43:17 -0500

    Update hw2.py

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,1 +1,12 @@
+import math
+import random
+
+frequency = float(input("Enter a value for the frequency, w: "))
+duration = float(input("Enter a value for the duration of the sound wave, T: "))
+
+fourier_transform = (2 * math.sin(frequency*duration))/(frequency)
+
+fourier_transform = round(fourier_transform,3)
+
+print("The amplitude spectrum of this Fourier transform is: " + str(fourier_transform))
 
