f5829775b66a4308dc79407e127d8720ce7044e7
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 18:51:44 -0500

    Update hw3.py

--- hw2/hw3.py
+++ hw2/hw3.py
@@ -1,1 +1,15 @@
 
+import math
+import random
+
+
+maximum_health = float(input("Enter the amount of health points does the Pokémon has! "))
+
+pokéball_value = random.randint(1,255)
+pokémon_current = random.randint(1,maximum_health)
+random_number = random.randint(0,255)
+
+
+formula_for_catch = ((maximum_health * 255 * 4)//(pokémon_current * pokéball_value))
+
+print(formula_for_catch >= random_number)
