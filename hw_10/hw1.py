86624eb511405e820784a84455617c03e639568a
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 19:14:11 -0500

    Update hw1.py

--- hw_10/hw1.py
+++ hw_10/hw1.py
@@ -1,1 +1,18 @@
+import random
 
+
+class Instrument:
+    def __init__(self, model, brand, strength):
+        self.model = model
+        self.brand = brand
+        self.strength = strength
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.strength} / 100 strength)"
+
+    def does_break(self):
+        random_value = random.random()
+        if random_value < (0.5 * self.strength):
+            return True
+        else:
+            return False
