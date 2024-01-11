1662ae7fd9ddee089fe75d8e0804f4ab1d2826a6
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 20:01:37 -0500

    Update hw3.py

--- hw5/hw3.py
+++ hw5/hw3.py
@@ -1,1 +1,16 @@
 
+letters = input("Please enter a string of lowercase letters: ")
+
+current_value = ord(letters[0])
+is_decreasing = True
+
+for i in range(1, len(letters)):
+    letter_value = ord(letters[i])
+    if (letter_value > current_value):
+        is_decreasing = False
+    current_value = letter_value
+
+if is_decreasing == True:
+    print(letters + " is decreasing")
+else:
+    print(letters + " is not decreasing")
