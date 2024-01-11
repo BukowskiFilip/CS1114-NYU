ed3c378472d37080081796b4f4d901e35cd925ca
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 19:53:12 -0500

    Update hw1.py

--- hw4/hw1.py
+++ hw4/hw1.py
@@ -1,1 +1,20 @@
+user_input = int(input("Please enter a positive integer: "))
+counter = 0
+value = 1
 
+print("Executing while-loop...")
+
+while counter < user_input:
+    print(value)
+    value += 2
+    counter += 1
+    
+print(" ")
+
+print("Executing for-loop...")
+
+value_2 = 1
+
+for number_2 in range(user_input):
+    print (value_2)
+    value_2 += 2
