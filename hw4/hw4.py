f51523196ecca415d276bfdd2ad5e066832724de
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 19:54:26 -0500

    Update hw4.py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,1 +1,17 @@
+answer = " "
 
+user_input = int(input("Input a positive integer: "))
+
+for number in range(1, user_input + 1):
+    even = 0
+    odd = 0
+    for digit in str(number):
+        number_2 = int(digit)
+        if number_2 % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    if even > odd:
+        answer = answer + str(number) + " "
+        
+print(answer)
