88e8903a86466bd0020e00c4bd93a6ea74beca38
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-10 19:54:09 -0500

    Update hw3.py

--- hw4/hw3.py
+++ hw4/hw3.py
@@ -1,1 +1,14 @@
 
+user_input = int(input("Input a positive integer: "))
+spacing = 0
+
+for steps in range((1),(user_input + 1)):
+    answer = ("")
+    user_input -= 1
+    spacing = ((user_input) * (" "))
+    
+    for counting in range(steps,0,-1):
+        answer += str(counting)
+        
+    print(spacing + answer)
+    
