13625989a68a32d0824f79145e1afe5ad45d108c
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-23 23:54:13 -0500

    Update hw1_q3.py

--- hw1/hw1_q3.py
+++ hw1/hw1_q3.py
@@ -1,1 +1,18 @@
+#question correct 2/2
 
+
+
+
+
+print("Please enter number of coins")
+user_quarters = int(input("Number of quarters: "))
+user_dimes = int(input("Number of dimes: "))
+user_nickels = int(input("Number of nickels: "))
+user_pennies = int(input("Number of pennies: "))
+
+total_money = ((user_quarters * 25)+(user_dimes * 10) + (user_nickels * 5) + (user_pennies))
+
+total_dollars = (total_money // 100)
+total_cents = (total_money % 100)
+
+print("The total is " + str(total_dollars) + " dollar(s) and " + str(total_cents) + " cent(s)")
