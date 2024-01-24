22b5f70eed565ce10867eacc6154e1534d386a1e
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-01-23 23:51:52 -0500

    Update hw1_q1.py

--- CS1114_HW1/hw1_q1.py
+++ CS1114_HW1/hw1_q1.py
@@ -1,1 +1,8 @@
+#no errors 2/2
 
+
+
+user_name = (input("Please enter your name: "))
+user_age = int(input("Please enter your age: "))
+
+print(user_name + ", " + str(user_age) + ", is taking CS-UY 1114")
